c89a093330f8712cf42f34443487a777c581acca
alvido <64369060+alvido@users.noreply.github.com>
2023-11-02 00:30:27 +0200

    reasons and reviews
    add section reasons and reviews

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,6 @@ $("[data-scroll]").on("click", function (event) {
 
 
 // Fixed header
-
 $(document).ready(function () {
   var header = $(".header"),
     headerH = header.innerHeight(),
@@ -113,3 +112,52 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 //
+
+
+// Slider
+$(document).ready(function () {
+  if ($('#slider__reviews').length) {
+    $('#slider__reviews').slick({
+      appendArrows: $('#reviews__arrows'),
+      prevArrow: `<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+      <path d="M19.0596 12L5.05957 12" stroke="#0F0E13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12.0596 5L5.05957 12L12.0596 19" stroke="#0F0E13" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></button>`,
+      nextArrow: `<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+      <path d="M5.05957 12L19.0596 12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12.0596 5L19.0596 12L12.0596 19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></button>`,
+      //appendDots: $('#city__dots'),
+
+      variableWidth: true,
+      centerMode: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: false,
+      arrows: true,
+      infinite: true,
+
+      responsive: [
+        {
+          breakpoint: 700,
+          settings: {
+            arrows: false,
+            slidesToShow: 2,
+            variableWidth: false,
+          },
+        },
+        {
+          breakpoint: 560,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            variableWidth: false,
+          },
+        },
+      ],
+    });
+  }
+});
+// slider
